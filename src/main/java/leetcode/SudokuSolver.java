ff80245da94b702e2bf7d0e3488277b5dd1cc917
Jacob <xuyankang@gmail.com>
2014-07-06 22:42:59 +0800

    do nothing

---
+++ src/main/java/leetcode/SudokuSolver.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+/**
+ * Created by Jacob on 2014/5/6.
+ */
+public class SudokuSolver {
+
+    public static void main(String[] args) {
+        SudokuSolver solution = new SudokuSolver();
+        System.out.println(3);
+    }
+
+    /**
+     * Write a program to solve a Sudoku puzzle by filling the empty cells.
+     * <p/>
+     * Empty cells are indicated by the character '.'.
+     * <p/>
+     * You may assume that there will be only one unique solution.
+     *
+     * @param board
+     */
+    public void solveSudoku(char[][] board) {
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                
+            }
+        }
+
+
+    }
+}
