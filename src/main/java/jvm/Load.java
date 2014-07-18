facf6375952085052a8d2f218e096ff498b9c314
XuYankang <yankang.xu@dianping.com>
2014-07-18 18:03:11 +0800

    aa

---
+++ src/main/java/jvm/Load.java
@@ -0,0 +1,10 @@
+package jvm;
+
+/**
+ * Created by Administrator on 2014/7/18.
+ */
+public class Load {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/jvm/Test.java
@@ -0,0 +1,15 @@
+package jvm;
+
+/**
+ * Created by Administrator on 2014/7/18.
+ */
+public class Test {
+    public Test() {
+        System.out.println("Hello");
+    }
+
+    public void say() {
+        System.out.println("world");
+
+    }
+}
