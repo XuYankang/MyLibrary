4930bf56bb2ad17c3e51afdbb43b986914e1462b
jacob <xuyankang@gmail.com>
2014-09-11 20:52:03 +0800

    simplist

--- src/main/java/other/PrintCodeAsHtml.java
+++ src/main/java/other/PrintCodeAsHtml.java
@@ -12,11 +12,22 @@ import java.util.List;
 public class PrintCodeAsHtml {
     public static void main(String[] args) throws IOException {
         ///Users/jacob/dev/workspace/MyLibrary/src/main/java/
-        File dir = new File("/Users/jacob/dev/workspace/MyLibrary/src/main/java/");
+        File dir = new File("/Users/jacob/dev/workspace/MyLibrary/src/main/java/leetcode/");
         StringBuilder stringBuilder = new StringBuilder();
         for (File file : dir.listFiles()) {
-            List<String> list = FileUtils.readLines(file);
-            
+            if (file.getName().startsWith(".") || file.isDirectory() || !file.getName().endsWith("java")) {
+                continue;
+            }
+            stringBuilder.append("<h1>" + file.getName() + "</h1><hr>");
+            List<String> list = FileUtils.readLines(file, "UTF-8");
+            for (String line : list) {
+                line = line.replace(" ","&nbsp;");
+                line = line.replace("\t","&bnsp;&bnsp;&bnsp;&bnsp;");
+                stringBuilder.append(line + "<br>");
+            }
+            stringBuilder.append("<hr>");
         }
+        System.out.println(stringBuilder);
+        FileUtils.writeStringToFile(new File("/Users/jacob/temp/leetcode.html"), stringBuilder.toString());
     }
 }
