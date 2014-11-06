425a9e120f0a8881f89a56bb2ce4ccc0160c6f14
jacob <xuyankang@gmail.com>
2014-11-06 15:18:37 +0800

    works applications

--- src/main/java/concurrency/Test.java
+++ src/main/java/concurrency/Test.java
@@ -1,5 +1,8 @@
 package concurrency;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -7,9 +10,44 @@ import java.util.concurrent.Executors;
  * Created by Jacob on 2014/6/18.
  */
 public class Test {
-    public static void main(String[] args) {
-        System.out.println(Runtime.getRuntime().availableProcessors());
-        ExecutorService threadPool = Executors.newCachedThreadPool();
 
+    static Random random = new Random();
+    static int count = 1;
+
+    public static void main(String[] args) {
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        int size = 1000000;
+        final CountDownLatch countDownLatch = new CountDownLatch(size);
+
+        final ArrayList<Double> list = new ArrayList<Double>();
+        for (int i = 0; i < size; i++) {
+            list.add(random.nextDouble());
+        }
+
+        long start = System.currentTimeMillis();
+
+        class Cal implements Runnable {
+            ArrayList<Double> list;
+            int i;
+
+            Cal(ArrayList<Double> list, int i) {
+                this.list = list;
+                this.i = i;
+            }
+
+            @Override
+            public void run() {
+                double v = list.get(i) * list.get(i);
+            }
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            executorService.submit(new Cal(list, i));
+        }
+        long end = System.currentTimeMillis();
+        System.out.println(end - start);
+        executorService.shutdown();
     }
 }
---
+++ src/main/java/test/Test.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * Created by jacob on 14/10/29.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(1<<3);
+    }
+}
